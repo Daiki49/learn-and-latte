d610d9438fcb24532ef8b2423411f81af1a0440b
pikattya <daiki.kato.tech@gmail.com>
2024-06-17 22:45:17 +0900

    検索機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.includes(:user)
+    @q = Post.ransack(params[:q])
+    @posts = @q.result(distinct: true).includes(:user).order(created_at: :desc)
   end
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,3 +1,6 @@
 class Shop < ApplicationRecord
     has_many :posts, dependent: :destroy
+    def self.ransackable_attributes(auth_object = nil)
+        ["address", "created_at", "id", "id_value", "latitude", "longitude", "name", "opening_hours", "phone_number", "place_id", "updated_at"]
+    end
 end
