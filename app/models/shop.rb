6d65345679138c3d776224512eaa62aa276fd3de
pikattya <daiki.kato.tech@gmail.com>
2024-07-23 07:05:31 +0900

    店舗検索機能（キーワード検索）の実装

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,7 +1,10 @@
 class Shop < ApplicationRecord
     has_many :posts, dependent: :destroy
     def self.ransackable_attributes(auth_object = nil)
-        ["address", "created_at", "id", "id_value", "latitude", "longitude", "name", "opening_hours", "phone_number", "place_id", "updated_at"]
+        ["name", "address"]
+    end
+    def self.ransackable_associations(auth_object = nil)
+        ["shop"]
     end
     validates :name, :address, presence: true
 end
