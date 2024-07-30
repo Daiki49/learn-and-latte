1df33255cad807db113a4c68320a6dd7c500e27a
pikattya <daiki.kato.tech@gmail.com>
2024-07-30 19:31:32 +0900

    高度なマーカーに移行

--- app/javascript/custom/googlemap.js
+++ app/javascript/custom/googlemap.js
@@ -9,27 +9,27 @@ let map;
 
 async function initMap() {
   // [START maps_add_map_instantiate_map]
-  // The location of Uluru
+  // The location of Shinjuku Station
   const position = { lat: 35.68978920591743, lng: 139.70055989948156 };
   // Request needed libraries.
   //@ts-ignore
   const { Map } = await google.maps.importLibrary("maps");
-  const { AdvancedMarkerView } = await google.maps.importLibrary("marker");
+  const { AdvancedMarkerElement, PinElement } = await google.maps.importLibrary("marker");
 
-  // The map, centered at Uluru
+  // The map, centered at Shinjuku Station
   map = new Map(document.getElementById("map"), {
-    zoom: 15,
+    zoom: 16,
     center: position,
     mapId: "DEMO_MAP_ID",
   });
 
   // [END maps_add_map_instantiate_map]
   // [START maps_add_map_instantiate_marker]
-  // The marker, positioned at Uluru
-  const marker = new AdvancedMarkerView({
+  // The marker, positioned at Shinjuku Station
+  const marker = new google.maps.marker.AdvancedMarkerElement({
     map: map,
     position: position,
-    title: "Uluru",
+    title: "Shinjuku Station",
   });
   // [END maps_add_map_instantiate_marker]
 }
