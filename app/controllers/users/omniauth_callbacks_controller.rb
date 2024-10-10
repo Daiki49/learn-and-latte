8bb2a61d0e5ea8b4afec47f4c2061ffce127f812
pikattya <daiki.kato.tech@gmail.com>
2024-10-11 00:22:03 +0900

    Googleログインの実装

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,6 +4,41 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   # You should configure your model like this:
   # devise :omniauthable, omniauth_providers: [:twitter]
 
+  # 通常、Railsではクロスサイトリクエストフォージェリ（CSRF）保護が有効
+  # しかし、Google OAuth 2.0のリクエストは外部から送られてくるため、CSRFトークンの検証は不要
+  # そのため、google_oauth2アクションに限って、CSRFトークンの検証をスキップ
+  # skip_before_action :verify_authenticity_token, only: :google_oauth2
+
+  def google_oauth2
+
+    # 返却される認証情報を利用して、ユーザーをデータベースに保存
+    # もしくはすでに存在するユーザーであればそのユーザーを取得
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      # 正常に保存され、データベースに存在する場合
+      # ユーザーをログインさせる
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
+    else
+      # 正常に保存されなかった場合
+      # セッションにomniauth.authのデータを一時的に保存
+      session["devise.google_data"] = request.env["omniauth.auth"].except("extra")
+      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
+    end
+  end
+
+  def failure
+    # 認証が失敗した場合
+    redirect_to root_path, alert: "Authentication failed, please try again."
+  end
+
+  private
+
+  def auth
+    # 返却された認証情報を取得
+    auth = request.env['omniauth.auth']
+  end
   # You should also create an action method in this controller like this:
   # def twitter
   # end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_25_125925) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_08_115101) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_25_125925) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
