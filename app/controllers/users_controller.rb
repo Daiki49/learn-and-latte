7cef460511ed4572fbe256db3ac1f157fc6355e4
pikattya <daiki.kato.tech@gmail.com>
2024-10-26 21:28:46 +0900

    マイページ画面作成(投稿一覧を追加）

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,23 @@
 class UsersController < ApplicationController
   def show
     @user = current_user
+    @posts = @user.posts.includes(:user).order(created_at: :desc) # ユーザーの投稿を取得
+    @no_results = @posts.empty? # 投稿がないかどうかを判断
   end
+
+  # def profile
+  #   @user = current_user
+  #   render partial: 'profile'
+  # end
+
+  # def my_posts
+  #   @user = current_user # 現在のユーザーを取得
+  #   @posts = @user.posts.includes(:user).order(created_at: :desc) # ユーザーの投稿を取得
+  #   @no_results = @posts.empty? # 投稿がないかどうかを判断
+
+  #   # @postsの内容をログに出力
+  #   Rails.logger.info "User posts: #{@posts.inspect}"
+
+  #   render partial: 'my_posts'
+  # end
 end
