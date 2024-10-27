8dd0d39de2bad789c1296d10633632cecb107929
pikattya <daiki.kato.tech@gmail.com>
2024-10-27 18:20:33 +0900

    レビュー修正・削除機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
 
   # 指定のアクションに対して認証を要求
   before_action :authenticate_user!, only: [:new, :create]
-  before_action :set_shop, only: [:new, :create]
+  before_action :set_shop, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @q = Post.ransack(params[:q])
@@ -26,6 +26,32 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+  end
+
+  def edit
+    @post = current_user.posts.find(params[:id])
+  end
+
+  def update
+    @post = current_user.posts.find(params[:id])
+    if @post.update(post_params)
+      # redirect_to shop_post_path(@shop, @post), success: t('defaults.flash_message.updated', item: post.model_name.human)
+      # TODO: 下記を使った形式に変更する。
+      # item: post.model_name.human
+      redirect_to shop_post_path(@shop, @post), success: t('defaults.flash_message.updated')
+    else
+      flash.now[:danger] = t('defaults.flash_message.not_updated', item: post.model_name.human)
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    post = current_user.posts.find(params[:id])
+    post.destroy!
+    # redirect_to posts_path, success: t('defaults.flash_message.deleted', item: post.model_name.human), status: :see_other
+    # TODO: 下記を使った形式に変更する。
+    # item: post.model_name.human
+    redirect_to shop_posts_path, success: t('defaults.flash_message.deleted'), status: :see_other
   end
 
   private
