caab3c1327832bd4af52d83c31fb5b4f49b438e9
pikattya <daiki.kato.tech@gmail.com>
2025-03-02 14:43:42 +0900

    マイページ画面の修正（タブでプロフィール・レビュー投稿一覧・いいね一覧が切り替えられるように修正）

--- config/routes.rb
+++ config/routes.rb
@@ -37,12 +37,7 @@ Rails.application.routes.draw do
   resources :bookmarks, only: %i[create destroy]
   resources :likes, only: %i[index create destroy]
 
-  resource :user, only: [:show] do
-    collection do
-      get 'profile', to: 'users#profile'
-      get 'likes', to: 'users#likes', as: :likes
-    end
-  end
+  resource :user, only: [:show]
 
   get 'images/ogp_image.png', to: 'images#ogp', as: 'images_ogp'
 end
